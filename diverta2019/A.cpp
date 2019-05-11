eb41beffa0a6a37ab953f46dfce6b809dd752c04
kinosi <kinosi.cat@gmail.com>
2019-05-11 23:44:26 +0900

    diverta2019

---
+++ diverta2019/A.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/diverta2019/tasks/diverta2019_a
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  int N, K;
+  cin >> N >> K;
+
+  cout << N - (K - 1) << endl;
+}
